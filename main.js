8090993d3ed08164d1b57d3cc238d11c6c755b62
abbir123 <86601912+abbir123@users.noreply.github.com>
2021-08-13 18:42:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+var canvas = new fabric.Canvas('myCanvas')
+
+ball_x=0;
+ball_y=0;
+goal_x=400;
+goal_y=800;
+
+
+
+
+
+function load_img(){
+    fabric.Image.fromURL(golf-h.png,function img	{
+	hole_obj= Img;
+	hole_obj.scaleToWidth (50);
+	hole_obj.scaleToHeight (50);
+	hole_obj.set({
+		top:hole_y
+		left
+	})
+	
+    
+
+}
+
+function new_image()
+{
+	// write code to Upload ball image on canvas
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	
+
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	
+	
+	function up()
+	{
+		
+	}
+
+	function down()
+	{
+		 // Write a code to move ball downward.
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
+}
+
